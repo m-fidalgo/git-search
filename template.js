9914e8f8ffbb9d8adc28a7d88b879422a0848794
Maira Fidalgo <mairaberlatof@gmail.com>
2021-05-13 19:34:13 -0300

    Proxy

--- template.js
+++ template.js
@@ -2,25 +2,39 @@ const languageSelect = document.querySelector("#language-tags");
 const listElement = document.querySelector("#list");
 const templateWorker = new Worker("./template-worker.js");
 
-const config = {
-  listItems: [],
-  languageTag: "en-US",
-};
+const config = new Proxy(
+  {
+    listItems: [],
+    languageTag: "en-US",
+  },
+  {
+    set: function (target, prop, value, receiver) {
+      if (prop === "listItems" || prop === "languageTag") {
+        Reflect.set(...arguments);
+        render();
+        return true;
+      }
+
+      return false;
+    },
+  }
+);
 
 languageSelect.addEventListener("change", changeLanguage);
 
 function changeLanguage() {
   config.languageTag = languageSelect.value;
-  render();
 }
 
 export function setList(list) {
   config.listItems = list;
-  render();
 }
 
 function render() {
-  templateWorker.postMessage(config);
+  //evita problemas por conta do proxy
+  const configParam = JSON.parse(JSON.stringify(config));
+
+  templateWorker.postMessage(configParam);
   templateWorker.onmessage = function ({ data }) {
     listElement.innerHTML = html;
   };
